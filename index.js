02329591cdd582e63ed7e2f8ca9e2f74072fee86
ezekielswanson <swansonezekiel@gmail.com>
2023-12-20 07:59:09 -0600

    Updates js

--- index.js
+++ index.js
@@ -2,29 +2,65 @@
 
 
 
-//Search Api
-/*
+/*Search Api
+
 get input value 
 get click eent on submit
 send api
 */
+
 document.querySelector('form button').addEventListener('click', (e) => {
 
     //Variables
     const movieTitle = document.querySelector('#searchInput').value
-
-    console.log(movieTitle)
     e.preventDefault()
 
+
+    //Fetching by search to return all serach results
     fetch(`http://www.omdbapi.com/?apikey=f89c6c72&s=${movieTitle}`)
     .then(res => res.json())
     .then(data => {
-        console.log(data)
+
+        //console.log(data)
+        //get run time, Ratings, Genre, 
+        data.Search.slice(0, 15).forEach(movie => {
+            //console.log(movie.imdbID)
+            const movieId = movie.imdbID
+            //console.log(movieId)
+
+            //Fetching by id to return id specific properties 
+            fetch(`http://www.omdbapi.com/?apikey=f89c6c72&t=${movieId}`)
+            .then(res => res.json())
+            .then(data => {
+
+                console.log(data)
+            
+            })
+            .catch(error => {
+                console.error('Error:', error);
+            });
+
+
+        })
+
+
+        
     })
     .catch(error => {
         console.error('Error:', error);
     });
+
+
+
+  
+
+
+
+
+
 })
+
+
 
 
 
