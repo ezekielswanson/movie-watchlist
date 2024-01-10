45864845ffa23f6c554bb116cd59c64cc8116ca1
ezekielswanson <swansonezekiel@gmail.com>
2024-01-09 21:18:06 -0600

    Updated watchlist js

--- index.js
+++ index.js
@@ -85,31 +85,5 @@ function removeFilmIcon() {
 }
 
 
-/*** Wathclist Js  ***/
 
-
-//Adding movie to local storage
-document.querySelector('.movie-list__body').addEventListener('click', (e) => {
-    const btn = e.target.closest('[data-movie-id]');
-
-    if (btn) {
-        const selectedMovie = btn.dataset.movieId; // Use 'btn' instead of 'e.target'
-        console.log(selectedMovie);
-
-        //access data id
-
-        /*
-        const movieObject = {
-            poster: 
-            title: 
-            rating: 
-            runtime: 
-            genre: 
-            plot: 
-
-        }
-        */
-
-    }
-});
 
