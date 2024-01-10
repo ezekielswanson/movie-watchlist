73427de65b73b8f3d058509d4da4a81e704dad61
ezekielswanson <swansonezekiel@gmail.com>
2024-01-09 21:17:33 -0600

    Updated js file

--- index.js
+++ index.js
@@ -48,7 +48,7 @@ document.querySelector('form button').addEventListener('click', async (e) => {
                             <div class="movie-list__row--text__info">
                                 <p>${movie.Runtime}</p>
                                 <p>${movie.Genre}</p>
-                                <button class="movie-list__row--text__info--btn" movie-id="${movie.imdbID}">
+                                <button class="movie-list__row--text__info--btn" data-movie-id="${movie.imdbID}">
                                     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 16 16" fill="none">
                                         <path fill-rule="evenodd" clip-rule="evenodd" d="M8 16C12.4183 16 16 12.4183 16 8C16 3.58172 12.4183 0 8 0C3.58172 0 0 3.58172 0 8C0 12.4183 3.58172 16 8 16ZM9 5C9 4.44772 8.55228 4 8 4C7.44772 4 7 4.44772 7 5V7H5C4.44772 7 4 7.44771 4 8C4 8.55228 4.44772 9 5 9H7V11C7 11.5523 7.44772 12 8 12C8.55228 12 9 11.5523 9 11V9H11C11.5523 9 12 8.55228 12 8C12 7.44772 11.5523 7 11 7H9V5Z" fill="#111827"/>
                                     </svg>
@@ -85,18 +85,18 @@ function removeFilmIcon() {
 }
 
 
-
-
-
-
 /*** Wathclist Js  ***/
 
 
 //Adding movie to local storage
 document.querySelector('.movie-list__body').addEventListener('click', (e) => {
+    const btn = e.target.closest('[data-movie-id]');
 
-    if (e.target.closest(".movie-list__row--text__info--btn")) {
-        console.log(e);
+    if (btn) {
+        const selectedMovie = btn.dataset.movieId; // Use 'btn' instead of 'e.target'
+        console.log(selectedMovie);
+
+        //access data id
 
         /*
         const movieObject = {
@@ -111,7 +111,5 @@ document.querySelector('.movie-list__body').addEventListener('click', (e) => {
         */
 
     }
-   
 });
-
 
