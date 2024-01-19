2b63b0812c3d6e8fbfc6d68c37bca2794b715e23
ezekielswanson <swansonezekiel@gmail.com>
2024-01-18 20:35:10 -0600

    Updated js

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-
-
 /*Search Api
 
 get input value 
@@ -65,7 +63,8 @@ document.querySelector('form button').addEventListener('click', async (e) => {
                 `;
             });
 
-            const listContainer = document.querySelector('.movie-list__body');
+
+            const listContainer  = document.querySelector('.movie-list__body');
             listContainer.innerHTML = movieList;
 
     
@@ -78,9 +77,70 @@ document.querySelector('form button').addEventListener('click', async (e) => {
 });
 
 
+
 //Removing film icon on load
 function removeFilmIcon() {
     const removeFilmIcon = document.querySelector('.movie-list__film-icon');
     removeFilmIcon.classList.add('remove');
 }
+
+
+
+
+/*** Watchlist Js ****/
+const addedMovies = [];
+
+
+
+
+
+
+//Adding movie to local storage
+document.querySelector('.movie-list__body').addEventListener('click', (e) => {
+    const btn = e.target.closest('[data-movie-id]');
+
+    if (btn) {
+        const selectedMovie = btn.dataset.movieId; 
+        console.log(selectedMovie);
+
+        //access data id
+
+        //how do I get access to the movie object used to created html?
+        const movieEl = btn.closest('.movie-list__row')
+        
+        
+        const movieObject = {
+            poster: movieEl.querySelector('img').src,
+            title: movieEl.querySelector('.movie-list__row--text__title h3').textContent,
+            rating: movieEl.querySelector('.movie-list__row--text__title p').textContent,
+            runtime: movieEl.querySelector('.movie-list__row--text__info p:first-child').textContent,
+            genre: movieEl.querySelector('.movie-list__row--text__info p:nth-child(2)').textContent,
+            plot: movieEl.querySelector('.movie-list__row--text__info--desc p').textContent,
+
+
+        }
+        
+
+        addedMovies.push(movieObject)
+
+
+    }
+
+    storeInLocalStorage(addedMovies)
+    console.log(addedMovies)
+    
+});
+
+
+
+
+
+function storeInLocalStorage(movieArray) {
+    const jsonString = JSON.stringify(movieArray)
+    localStorage.setItem('movieArray',jsonString );
+
+}
+
+
+
 
